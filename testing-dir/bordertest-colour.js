68dd07eb1683b9b793f5d1abb91fd93d4cbcde60
NtGm 889 <48107185+NtGm-889@users.noreply.github.com>
2023-09-22 19:09:07 +1200

    Test pages
    idk, experiments?

---
+++ testing-dir/bordertest-colour.js
@@ -0,0 +1,24 @@
+function recolourRed() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#ff9999, #cc0000, #ff9999);";
+}
+function recolourOrange() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#ffcc99, #ff9933, #ffcc99);";
+}
+function recolourYellow() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#ffff99, #ecb613, #ffff99);";
+}
+function recolourGreen() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#66ff99, #339966, #66ff99);";
+}
+function recolourBlue() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#3399ff, #0033cc, #3399ff);";
+}
+function recolourPurple() {
+	document.getElementById("window").style.cssText =
+		"background: linear-gradient(#cc99ff, #6600ff, #cc99ff);";
+}
